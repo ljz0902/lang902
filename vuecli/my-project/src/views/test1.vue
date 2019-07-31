0e363388797f2b8233e7b8527cb6057699fcf474
ljz0902 <951057727@qq.com>
2019-07-31 16:12:01 +0800

    admin

--- vuecli/my-project/src/views/test1.vue
+++ vuecli/my-project/src/views/test1.vue
@@ -4,3 +4,15 @@
         <h1>姓名:{{$route.params.age}}</h1>
     </div>
 </template>
+<script>
+export default {
+    beforeRouterEnter(to,from,next){
+        console.log(to,from);
+        next(true)
+    },
+    beforeRouterLeave(to,from,next){
+        console.log(to,from);
+        next(true)
+    }
+}
+</script>
