e944f5c3b9cbc60043be4c2ec9fc8a4ac7ad24d4
ljz0902 <951057727@qq.com>
2019-08-09 10:39:38 +0800

    admin

--- reactapp/my-app/src/App.js
+++ reactapp/my-app/src/App.js
@@ -8,26 +8,22 @@ import {
   Switch,
   Redirect
  } from "react-router-dom";
-
+import Test1 from"./component/Test1";
+import Test2 from"./component/Test2";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Link to="/">test1</Link>
+        <Link to="/test2">test2</Link>
+        <Switch>
+          <Route path="/test2" component={Test2}/>
+          <Route path="/" componment={Test1}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
