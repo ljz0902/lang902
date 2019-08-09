407f2374d4cecf5d50201942eddcf19720a6c099
ljz0902 <951057727@qq.com>
2019-08-09 11:09:52 +0800

    admin

--- reactapp/my-app/src/App.js
+++ reactapp/my-app/src/App.js
@@ -2,27 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
-  BrowserRouter as Router,
-  Route,  
-  Link,
-  Switch,
-  Redirect
- } from "react-router-dom";
-import Test1 from"./component/Test1";
-import Test2 from"./component/Test2";
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch,
+    Redirect
+} from "react-router-dom";
+import Test1 from "./component/Test1";
+import Test2 from "./component/Test2";
+const Foo = ({match}) => {
+    return (
+        <h1>hello{match.params.id}</h1>
+    )
+}
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Link to="/">test1</Link>
-        <Link to="/test2">test2</Link>
-        <Switch>
-          <Route path="/test2" component={Test2}/>
-          <Route path="/" componment={Test1}/>
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Link to="/54321">test1</Link>
+                <Link to="/test2">test2</Link>
+                <Link to="/foo/123456">foo</Link>
+                <Switch>
+                    <Route path="/foo/:id" componment={Foo} />
+                    <Route path="/test2" component={Test2} />
+                    <Route path="/:id" componment={Test1} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
