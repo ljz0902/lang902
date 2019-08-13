105eb99ced22362824eb1650cf418f0e4455552d
ljz0902 <951057727@qq.com>
2019-08-13 16:23:08 +0800

    admin

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -4,29 +4,36 @@ const cors = require('koa2-cors');
 const Router = require('koa-router');
 const koaBody = require('koa-body')
 const cloud = require('tcb-admin-node');
-
+//初始化路由
 const router = new Router({
-    prefix:"/fellow"
+    prefix: "/fellow"
 });
-
+//跨域处理
 app.use(cors({
     // origin: ['http://localhost:9528'],
-    origin: function(ctx) {
-      return [ctx.request.header.origin];
+    origin: function (ctx) {
+        return [ctx.request.header.origin];
     },
     maxAge: 5,
     credentials: true
 }));
-
+//请求参数格式化
 app.use(koaBody());
 
+//小程序初始化
 cloud.init({
-    secretId: 'AKIDvZ2pSGCZ37BOzhc0fCp7VhYHEctWOJF4',
-    secretKey: 'Bimp3SqhT5caboRTMC322bltPxKvcSIJ',
-    env:"lang-yv4or"
-    // env:'test-41b8dc'   
+    secretId: 'AKIDwh03sRhJbfVAamvTyEKCsITO5XkhcwEI',
+    secretKey: 'xgAPPBk5Azby6yd5JJ5dz6yfBRSDk7F7',
+    env: "lang-yv4or"
 });
 
+
+let user = require('./controller/user.js');
+router.use('/user', user.routes());
+
+//使用router
 app.use(router.routes());
 
+//创建监听 端口号
 app.listen(3000);
+console.log("server runing")
